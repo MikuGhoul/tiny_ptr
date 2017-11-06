2335709e05c2be7bb90b8bae64856dde99bdd07a
MikuGhoul <mikughoull@gmail.com>
2017-11-06 22:00:15 +0800

    preprocessor && namespace

--- test.cpp
+++ test.cpp
@@ -8,13 +8,13 @@ struct Key {
 
 int main(int argc, char *argv[])
 {
-    tiny_ptr<int> test_1 = make_ptr<int>(199);
+    tiny::tiny_ptr<int> test_1 = tiny::make_ptr<int>(199);
 
-    tiny_ptr<Key> test_2(new Key);
-    tiny_ptr<Key> test_3 = test_2;
-    tiny_ptr<Key> test_4(test_3);
+    tiny::tiny_ptr<Key> test_2(new Key);
+    tiny::tiny_ptr<Key> test_3 = test_2;
+    tiny::tiny_ptr<Key> test_4(test_3);
 
-    tiny_ptr<int> test_5(new int(200));
+    tiny::tiny_ptr<int> test_5(new int(200));
 
     std::cout << test_2.use_count() << std::endl
               << (*test_2).value << std::endl
@@ -24,7 +24,6 @@ int main(int argc, char *argv[])
     test_1.reset();
     test_1.reset(new int(299));
     test_1.swap(test_5);
-
     
     std::cout << *test_1 << std::endl;
 }
